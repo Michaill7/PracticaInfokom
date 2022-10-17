531949b27ff071989b9f55f0af3c9f1da02f469e
Michaill <mix24102410@gmail.com>
2022-10-17 05:28:21 +0300

    end 100%

--- PracticaInfokom/ViewModels/MainWindowViewModel.cs
+++ PracticaInfokom/ViewModels/MainWindowViewModel.cs
@@ -63,29 +63,33 @@ namespace PracticaInfokom.ViewModels
         private bool UpdateButtonCanExecute(object o) => true;
         private void UpdateBUttonOnExecuted(object o)
         {
+            //Получение даты и времени с интерфейса и конвертирование в DateTime
+            DateTime upd_begin;
+            DateTime.TryParse(DateBegin, out upd_begin);
+            DateTime upd_end;
+            DateTime.TryParse(DateEnd, out upd_end);
+            //Открытие подключения к БД и запрос нужных полей с условием даты и времени
             using (var connection = new SqlConnection("Data Source=DESKTOP-STGMEUB;Initial Catalog=DBPractica;Integrated Security=True"))
             {
-                DateTime upd_begin;
-                DateTime.TryParse(DateBegin, out upd_begin);
-                DateTime upd_end;
-                DateTime.TryParse(DateEnd, out upd_end);
                 connection.Open();
-                SqlCommand com = new SqlCommand($"SELECT \"Дата-время начала (план)\", \"№ задания\", \"№ ПЗ\", \"Толщина (план)\", \"Ширина (план)\", \"Вес (план)\", \"Длина (ПЗ)\", \"Длина (по разметке)\" FROM Practica WHERE \"Дата-время начала (план)\" >= '{upd_begin}' AND \"Дата-время начала (план)\" <='{upd_end}'", connection);
-                SqlDataReader reader = com.ExecuteReader();
-                DataTable dt = new DataTable();
+                var com = new SqlCommand($"SELECT \"Дата-время начала (план)\", \"№ задания\", \"№ ПЗ\", \"Толщина (план)\", \"Ширина (план)\", \"Вес (план)\", \"Длина (ПЗ)\", \"Длина (по разметке)\" FROM Practica WHERE \"Дата-время начала (план)\" >= '{upd_begin}' AND \"Дата-время начала (план)\" <='{upd_end}'", connection);
+                var reader = com.ExecuteReader();
+                //Создание DataTable и загрузка в нее данных из запроса, после чего связка ее со свойством DataSlab
+                var dt = new DataTable();
                 dt.Load(reader);
                 Slyabs = dt;
-
+                //Аналогичные действия для таблицы кратов
                 com = new SqlCommand($"select \"№ ПЗ\", \"Длина (по разметке)\", \"Ширина (план)\", \"Вес (план)\", Round(\"Длина (Пз)\"/TRY_CAST(\"Длина (по разметке)\" AS FLOAT),0) AS \"Количество\" FROM Practica as P WHERE \"Дата-время начала (план)\" >= '{upd_begin}' AND \"Дата-время начала (план)\" <='{upd_end}'", connection);
                 reader = com.ExecuteReader();
                 dt = new DataTable();
                 dt.Load(reader);
                 Crats = dt;
-
+                //Закрытие подключения и связанных ресурсов
                 connection.Close();
-                connection.Dispose();
+
             }
         }
+    
 
         public MainWindowViewModel()
         {
